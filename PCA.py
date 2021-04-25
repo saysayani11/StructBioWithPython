55d1273dcef65a563417d2df8490b1a64d43b5e9
sayani <70441269+saysayani11@users.noreply.github.com>
2021-04-25 15:35:21 +0530

    Update PCA.py

--- PCA.py
+++ PCA.py
@@ -10,25 +10,30 @@ import seaborn as sns; sns.set()
 # The dataset used is of size (270 X 19) and comprises the relative C-alpha backbone distances of different PDB structures of the human CK2 protein to a reference CK2 protein
 # Load the prepared dataset; 
 # Use <df>.shape to obtain the shape of the dataset
-dist_data = pd.read_csv(r"C:\Users\saySa\OneDrive\Desktop\RMSD_data.csv")
+df = pd.read_csv(r"C:\Users\saySa\OneDrive\Desktop\RMSD_data.csv")
+
+#Standardise the dataset
+df_std= (df-df.mean()) / (df.std()) 
 
 # Use PCA to project data to a more manageable number of dimensions, say two
 # Set the number of Principal Components
-pca = PCA(2)  
-projected = pca.fit_transform(dist_data)
+# Fit the model and apply the dimensionality reduction on the dataset
+# [Features_matrix] (270*19)  X  [Top_Eigenvector_Matrix] (19*2)  =  [Transformed_matrix] (270*2)
+
+pca = PCA(n_components=2, svd_solver='full') 
+projected = pca.fit_transform(df_std)
 
 # Check the projected data's dimensions, reduced to the specified number of components
 # print(dist_data.shape)
 # print(projected.shape)
 # original shape:    (270,19)
-# transformed shape: (200,2)
+# transformed shape: (270,2)
 
 # Select a "target" column from the original dataset
-y = dist_data['3bqc (30-300)']
+y = df_std['3bqc (30-300)']
 
 # We can now plot the first two principal components of each point to learn about the data
 plt.scatter(projected[:, 0], projected[:, 1],  c=y, edgecolor='none', alpha=0.7,cmap=plt.cm.get_cmap('nipy_spectral', 10))
 plt.xlabel('Component 1')
 plt.ylabel('Component 2')
 plt.colorbar();
-
